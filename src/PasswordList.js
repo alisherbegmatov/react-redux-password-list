ef7e01f74eaf4450c084b4d95446c04ecf26d739
Alisher Begmatov <60242355+alisherbegmatov@users.noreply.github.com>
2021-06-21 16:00:15 -0700

    Initial commit

---
+++ src/PasswordList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class PasswordList extends Component {
+  getList () {
+    return this.props.passwords.map((pass, index) => {
+      return (
+        <tr key={index}>
+        <td>{pass.name}</td>
+        <td className='password-text'>{pass.password}</td>
+        </tr>
+      )
+    })
+  }
+
+  render () {
+    return (
+      <div className='password-table'>
+      <table>
+      <tr>
+      <th>NAME</th>
+      <th>PASSWORD</th>
+      </tr>
+      {this.getList()}
+      </table>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    passwords: state.passwords
+  }
+}
+
+export default connect(mapStateToProps)(PasswordList)
